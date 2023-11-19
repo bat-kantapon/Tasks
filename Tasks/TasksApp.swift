68f3c48920c0f3e526136e5e2e36c2b2551605e3
6310520066 <6310520066@student.tu.ac.th>
2023-11-19 19:24:32 +0700

    Initial Commit

---
+++ Tasks/TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksApp.swift
+//  Tasks
+//
+//  Created by Kantapon Makwong on 19/11/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
