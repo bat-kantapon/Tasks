6dc772def20ae7d6fad50d47b5b755fd74a2f361
bat-kantapon <bat.kantapon@gmail.com>
2023-12-14 16:06:09 +0700

    Complete Add note to RealtimeDB

--- Tasks/TasksApp.swift
+++ Tasks/TasksApp.swift
@@ -1,10 +1,29 @@
 import SwiftUI
+import FirebaseCore
+import FirebaseFirestore
+import Firebase
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct TasksApp: App {
+//    init() {
+//        FirebaseApp.configure()
+//    }
+    //@UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+
--- Tasks/ViewModels/NoteViewModel.swift
+++ Tasks/ViewModels/NoteViewModel.swift
@@ -8,9 +8,9 @@
 import Foundation
 import Alamofire
 import Combine
-
-import FirebaseCore
+import Firebase
 import FirebaseFirestore
+import FirebaseDatabase
 
 struct NoteResponse: Decodable {
     var id: Int
@@ -21,48 +21,52 @@ struct NoteResponse: Decodable {
 class NoteViewModel: ObservableObject {
     @Published private(set) var userAddedNotes: [Note] = []
     @Published private(set) var fetchedNotes: [Note] = []
+    private var ref: DatabaseReference!
+    
+    init() {
+        FirebaseApp.configure()
+        ref = Database.database().reference()
+        fetchNotes()
+    }
     
     func addOrUpdateUserAddedNote(_ note: Note) {
-            if let index = userAddedNotes.firstIndex(where: { $0.id == note.id }) {
-                userAddedNotes[index] = note
-            } else {
-                var newNote = note
-                newNote.id = UUID()
-                userAddedNotes.append(newNote)
-            }
+        if let index = userAddedNotes.firstIndex(where: { $0.id == note.id }) {
+            userAddedNotes[index] = note
+        } else {
+            var newNote = note
+            newNote.id = UUID()
+            userAddedNotes.append(newNote)
+            saveNoteToFirebase(newNote)
         }
-
-        func deleteNoteUserAdded(at offsets: IndexSet) {
-            userAddedNotes.remove(atOffsets: offsets)
-        }
+    }
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
-            // [START default_firestore]
-            FirebaseApp.configure()
-
-            let db = Firestore.firestore()
-            // [END default_firestore]
-
-            print(db) // silence warning
-
-            return true
-        }
+    func deleteNoteUserAdded(at offsets: IndexSet) {
+        userAddedNotes.remove(atOffsets: offsets)
+    }
     
     func fetchNotes() {
-            AF.request("https://jsonplaceholder.typicode.com/posts")
-                .validate(statusCode: 200..<300)
-                .responseDecodable(of: [NoteResponse].self) { response in
-                    switch response.result {
-                                    case .success(let notesResponse):
-                                        let fetchedNotes = notesResponse.map { noteResponse in
-                                            return Note(title: noteResponse.title, content: noteResponse.body)
-                                        }
-                                        self.fetchedNotes = fetchedNotes
-                                    case .failure(let error):
-                                        print("Error fetching notes: \(error)")
-                                    }
-                }
-        }
+        AF.request("https://jsonplaceholder.typicode.com/posts")
+            .validate(statusCode: 200..<300)
+            .responseDecodable(of: [NoteResponse].self) { response in
+                switch response.result {
+                    case .success(let notesResponse):
+                        let fetchedNotes = notesResponse.map { noteResponse in
+                            return Note(title: noteResponse.title, content: noteResponse.body)
+                        }
+                        self.fetchedNotes = fetchedNotes
+                    case .failure(let error):
+                        print("Error fetching notes: \(error)")
+                    }
+            }
+    }
     
+    private func saveNoteToFirebase(_ note: Note) {
+        let noteRef = ref.child("notes").childByAutoId()
+        noteRef.setValue([
+            "id": note.id.uuidString,
+            "title": note.title,
+            "content": note.content
+        ])
+    }
+
 }
